11c131ebfba38257325a0b0edd1587f970e38f28
Robin Naundorf <r.naundorf@fh-muenster.de>
2016-06-13 16:05:24 +0200

    tet

--- lint.sh
+++ lint.sh
@@ -3,5 +3,6 @@ FILES=`find . -name "Dockerfile"`
 for file in $FILES
 do
   docker run --rm --privileged -v `pwd`:/root/ projectatomic/dockerfile-lint dockerfile_lint -r basic_rules.yaml -f "$file" ';' > /dev/null
+  echo $?
 done
 exit 1
